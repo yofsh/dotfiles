976bcd4f56ace0eeaa33671bd9c48e755946472b
Oleksandr Yaroshenko <to@yof.sh>
2024-09-01 21:55:45 +0200

    fixed lens image search

--- bin/search_image.sh
+++ bin/search_image.sh
@@ -4,7 +4,7 @@ IMAGE="/tmp/gsimg.$EXT"
 GEOMETRY="$(slurp)"
 
 if [ -z "$GEOMETRY" ]; then
-	exit 1
+  exit 1
 fi
 
 echo "$GEOMETRY" "$IMAGE"
@@ -12,13 +12,13 @@ echo "$GEOMETRY" "$IMAGE"
 grim -t $EXT -g "$GEOMETRY" -q 60 "$IMAGE"
 
 if [ ! -f "$IMAGE" ]; then
-	echo "No image to search, exiting."
-	exit 1
+  echo "No image to search, exiting."
+  exit 1
 fi
 
 notify-send -i $IMAGE -u low "Sending image to google"
-GOOGLEURL=$(curl -i -s -F sch=sch -F "encoded_image=@$IMAGE" https://lens.google.com/upload | grep -oP '(?<=url=).*?(?=")')
-URL="https://lens.google.com$GOOGLEURL"
+RESP=$(curl -i -s -F sch=sch -F "encoded_image=@$IMAGE" https://lens.google.com/upload)
+URL=$(echo $RESP | grep -oP '(?<=location: )https?://\S+')
 echo URL:
 echo $URL
 rm $IMAGE
