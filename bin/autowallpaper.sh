398a1bb13a5f8644de30fba6fbb8f444c80e9d63
Oleksandr Yaroshenko <to@yof.sh>
2024-09-11 15:38:52 +0200

    updated hypr to the latest config

--- bin/autowallpaper.sh
+++ bin/autowallpaper.sh
@@ -1,30 +1,22 @@
 #/bin/sh
-#
 WPDIR=/home/fobos/pics/wallpapers
-username=fobos
+monitor=($(hyprctl monitors | grep Monitor | awk '{print $2}'))
 
-monitor=(`hyprctl monitors | grep Monitor | awk '{print $2}'`)
-wal=$(find ${WPDIR} -name '*' | awk '!/.git/' | tail -n +2 | shuf -n 1)
-cache=""
+echo "Motitors are: $monitor"
 
-if [ -d ${WPDIR} ]; then
-  cd ${WPDIR}
-  git pull
-else
-  git clone ${wallpaperGit} ${WPDIR}
-  chown -R ${username}:users ${WPDIR}
-fi
-
+hyprctl hyprpaper unload all
+prev=""
 while true; do
-  if [[ $cache == $wal ]]; then
-    wal=$(find ${WPDIR} -name '*' | awk '!/.git/' | tail -n +2 | shuf -n 1)
-  else
-    cache=$wal
-    hyprctl hyprpaper unload all
-    hyprctl hyprpaper preload $wal
-    for m in ''${monitor[@]}; do
-      hyprctl hyprpaper wallpaper "$m,$wal"
-    done
-  fi
+  wal=$(find ${WPDIR} -name '*' | awk '!/.git/' | tail -n +2 | shuf -n 1)
+
+  echo "Preloading new"
+  hyprctl hyprpaper preload $wal
+  for m in ''${monitor[@]}; do
+    echo "Setting $wal, for $m"
+    hyprctl hyprpaper wallpaper "$m,$wal"
+  done
+  echo "Unload prev"
+  hyprctl hyprpaper unload "$prev"
+  prev=$wal
   sleep 900
 done
